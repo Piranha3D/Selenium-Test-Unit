9e600c2cc6ebf1fc66e6291a5f4fbeddcd0f4c44
Ghifary <dartvader500@gmail.com>
2025-02-22 22:57:08 +0700

    Add files via upload

---
+++ saucedemo.js
@@ -0,0 +1,41 @@
+const { Builder, By, Key, until } = require("selenium-webdriver");
+const assert = require("assert");
+
+async function saucedemoLoginTest() {
+    // Membuat koneksi dengan webdriver
+    let driver = await new Builder().forBrowser("chrome").build();
+
+    // Exception Handling & Conclusion
+    try {
+        // Buka URL di browser
+
+        await driver.get("https://saucedemo.com");
+
+        await driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        await driver
+            .findElement(By.xpath("//input[@id='password']"))
+            .sendKeys("secret_sauce");
+
+        await driver.findElement(By.name("login-button")).click();
+
+        // Add to cart
+        await driver.findElement(By.name("add-to-cart-sauce-labs-backpack")).click();
+
+        // Add to cart 2
+        await driver.findElement(By.name("add-to-cart-sauce-labs-bike-light")).click();
+      
+
+        // Assertion
+        let titleText = await driver.findElement(By.css(".app_logo")).getText();
+        assert.strictEqual(
+            titleText.includes("Swag Lab"),
+            true,
+            "Judul halaman tidak sesuai"
+        );
+
+    } finally {
+        
+    }
+}
+
+saucedemoLoginTest();
